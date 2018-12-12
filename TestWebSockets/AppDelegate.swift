8e2990ba4d3960ebd15409bb0cddd099dad793ed
frcocoatst <friedrichhaeupl@friedrichs-macbook-pro.local>
2018-12-12 15:07:55 +0100

    Initial Commit

---
+++ TestWebSockets/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestWebSockets
+//
+//  Created by Friedrich HAEUPL on 12.12.18.
+//  Copyright © 2018 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestWebSockets/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestWebSockets
+//
+//  Created by Friedrich HAEUPL on 12.12.18.
+//  Copyright © 2018 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
